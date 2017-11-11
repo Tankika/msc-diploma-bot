9fb1df2be127f78702357dbe684d9bc02be8b023
Tamás Perlaki <pt1029@gmail.com>
2017-11-11 21:23:28 +0100

    Event logging feature added

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ const net = require('net');
 const moment = require('moment');
 const _ = require('lodash');
 
-const NICK = 'tankikabot',
-    PING_PATTERN = /PING :([a-zA-Z\\.]+)/;
+function Bot(channel, eventLogger) {
 
-function Bot(channel) {
-    
+    const NICK = 'tankikabot';
+    const PING_PATTERN = /PING :([a-zA-Z\\.]+)/;
     const COMMAND_PATTERN = new RegExp(`PRIVMSG #${channel} :!(.*)\\s*$`);
 
-    let commands = {};
-    let timers = {};
-    let aliases = {};
+    const commands = {};
+    const timers = {};
+    const aliases = {};
+
     const client = net.connect({
-            port: 6667,
-            host: 'irc.chat.twitch.tv'
-        }, () => {
-            console.log(`${channel}: Connected`);
-            send('PASS oauth:9lnozel9qqgasq4cs1tc405c6weixg');
-            send(`NICK ${NICK}`);
-            send(`JOIN #${channel}`);
-        });
+        port: 6667,
+        host: 'irc.chat.twitch.tv'
+    }, () => {
+        send('PASS oauth:9lnozel9qqgasq4cs1tc405c6weixg');
+        send(`NICK ${NICK}`);
+        send(`JOIN #${channel}`);
+
+        eventLogger.info(`Joined chat room`, {channel: channel});
+    });
 
     client.on('data', (data) => {
-        console.log(`${channel}: Data arrived: ${data.toString()}`);
+        eventLogger.debug(`Data arrived`, {channel: channel, data: data.toString()});
     });
 
     client.on('data', (data) => {
@@ -31,7 +32,7 @@ function Bot(channel) {
         if(matches) {
             var name = matches[1];
             if(name in commands) {
-                console.log(`${channel}: Matched command: ${name}`);
+                eventLogger.debug('Matched command', {channel: channel, command: name});
                 sendMessage(commands[name]);
             } else if(name in aliases) {
                 const commandName = aliases[name];
@@ -40,12 +41,10 @@ function Bot(channel) {
                     throw new Error(`${channel}: ${commandName} was not found, associated with alias ${name}`);
                 }
 
-                console.log(`${channel}: Matched alias: ${name} for command: ${commandName}`);
+                eventLogger.debug('Matched alias', {channel: channel, alias: name, command: commandName});
                 sendMessage(commands[commandName]);
             } else {
-                const message = `Unknown command: ${name}`;
-                console.log(`${channel}: ${message}`);
-                sendMessage(message);
+                eventLogger.debug('Unknown command', {channel: channel, command: name});
             }
         }
     });
@@ -53,104 +52,84 @@ function Bot(channel) {
     client.on('data', (data) => {
         var matches = PING_PATTERN.exec(data);
         if(matches) {
-            console.log(`${channel}: PONG`);
+            eventLogger.debug('PONG', {channel: channel});
             send(`PONG ${NICK}, ${matches[1]}`);
         }
     });
 
     client.on('end', () => {
-        console.log(`${channel}: Disconnected`);
+        eventLogger.warn('Left chat room', {channel: channel, channel: channel});
     });
 
     this.setCommand = setCommand;
     this.removeCommand = removeCommand;
-    this.resetCommands = resetCommands;
     this.runCommand = runCommand;
     this.setTimer = setTimer;
     this.removeTimer = removeTimer;
     this.setAlias = setAlias;
     this.removeAlias = removeAlias;
-
+    
     function send(message) {
         client.write(`${message}\r\n`);
     }
-
+    
     function sendMessage(message) {
         client.write(`PRIVMSG #${channel} :${message}\r\n`);
     }
-
+    
     function setCommand(name, text) {
-        console.log(`${channel}: Adding command: !${name} - ${text}`);
-
         commands[name] = text;
     }
-
+    
     function removeCommand(name) {
         if(!name in commands) {
             return;
         }
-
-        console.log(`${channel}: Removing command: !${name}`);
+    
         delete commands[name];
     }
-
-    function resetCommands() {
-        console.log(`${channel}: Resetting commands`);
-        commands = {};
-    }
-
+    
     function runCommand(name) {
-        console.log(`${channel}: Running command: ${name}`);
-
         if(!name in commands) {
             throw new Error(`Command ${name} is not added to channel ${channel} or not enabled!`);
         }
-
+    
         sendMessage(commands[name]);
     }
     
     function setTimer(name, timeInMinutes, commandNames) {
-        console.log(`${channel}: Adding timer: ${name}`);
-
         if(name in timers) {
-            console.log(`${channel}: Timer(${name}) already exists, removing first`);
+            eventLogger.debug('Timer already exists, removing first', {channel: channel, timer: name});
             removeTimer(name);
         }
-
+    
         const intervalObject = setInterval(() => {
-            console.log(`${channel}: Timer triggered: ${name}`);
-
+            eventLogger.debug('Timer triggered', {channel: channel, timer: name});
+    
             _.each(commandNames, commandName => runCommand(commandName))
         }, moment.duration(timeInMinutes, 'minutes').asMilliseconds()); 
         
         timers[name] = intervalObject;
-
-        console.log(`${channel}: Added timer: ${name}`);
     }
-
+    
     function removeTimer(name) {
         if(!name in timers) {
             return;
         }
-
-        console.log(`${channel}: Removing timer: ${name}`);
-
+    
         clearInterval(timers[name]);
         delete timers[name];
     }
-
+    
     function setAlias(name, commandName) {
-        console.log(`${channel}: Adding alias: !${name} - !${commandName}`);
-
         aliases[name] = commandName;
     }
-
+    
     function removeAlias(name) {
         if(!name in aliases) {
             return;
         }
 
-        console.log(`${channel}: Removing alias: ${name}`);
         delete aliases[name];
     }
 }
